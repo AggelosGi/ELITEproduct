5b5f46d872e4ecf52c7bc0f5a5182c82831bcc91
AggelosGi <83583424+AggelosGi@users.noreply.github.com>
2021-05-28 17:47:46 +0300

    Add files via upload

--- Removal.java
+++ Removal.java
@@ -1,4 +1,18 @@
 
 public class Removal {
 
+	private int evaluations;
+	private int counter_deposit;
+	private int notice;
+	
+	
+	public selectRemoval() {
+		
+	}
+	
+	public selectProducersRemoval() {
+		
+	}
+	
+	
 }
