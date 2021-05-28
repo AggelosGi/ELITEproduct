7e30244b256313146ec1ad37f5a3085081f02cc9
AggelosGi <83583424+AggelosGi@users.noreply.github.com>
2021-05-28 22:25:40 +0300

    Add files via upload

--- Payment.java
+++ Payment.java
@@ -1,13 +1,22 @@
+import java.time.LocalDateTime;
 
 public class Payment {
 
-	private string card_name;
-	private int card_id;
-	private int cvv;
-	private DateTime date;
-	private string paypal_mail;
-	private string pass;
-	private double delivery_charges;
+	private string card_number; //the card number
+	
+	private string card_name; //the name of the card holder
+	
+	private int card_id; //the card id for later use
+	
+	private int cvv; //the validation code for the card. Supported for payments but not for saving payment cards for future use
+	
+	private LocalDateTime expiredate; //the expire date of the card 
+	
+	private string paypal_mail; //the mail to log in to paypal account
+	
+	private string pass; // the password for the paypal account
+	
+	private double delivery_charges; //pay on delivery charges
 	
 	
 	public retCreditCardFields () {
