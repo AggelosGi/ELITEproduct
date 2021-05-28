945c0869c7288718a9e359237bdebc1be6c0ff31
Nektariaa <83583290+Nektariaa@users.noreply.github.com>
2021-05-28 14:50:21 +0300

    Add files via upload

--- Buyer.java
+++ Buyer.java
@@ -1,4 +1,58 @@
 
 public class Buyer {
 
+	private String purchased_products [];
+	
+	
+	addOrder (){
+		
+	}
+	
+	productResume(){
+		
+	}
+	
+	selectDescAssortment(){
+		
+	}
+	
+	selectProduct(){
+		
+	}
+	
+	selectIncreAssortment(){
+		
+	}
+	
+	selectPayment(){
+		
+	}
+	
+	fillinFields(){
+		
+	}
+	
+	selectOrderEntry(){
+		
+	}
+	
+	selectContShopping(){
+		
+	}
+	
+	selectCheckout(){
+		
+	}
+	
+	selectBackatBuyersMainMenu(){
+		
+	}
+	
+	selectProductEvaluation(){
+		
+	}
+	
+	selectLeaveComment(){
+		
+	}
 }
