942fbe3391293a144b6441991b7d83909f076698
Nektariaa <83583290+Nektariaa@users.noreply.github.com>
2021-05-28 18:00:02 +0300

    Add files via upload

--- Order.java
+++ Order.java
@@ -1,4 +1,30 @@
 
 public class Order {
 
+	public string shopping_list [];
+	
+	
+	public retBuyList() {
+		
+	}
+	
+	private updateBuyList() {
+		
+	}
+	
+	public retOrderSummary () {
+		
+	}
+	
+	public sendSummaryViaMail() {
+		
+	}
+	
+	public selectDesirableQuantity() {
+		
+	}
+	
+	public selectQuantity() {
+		
+	}
 }
