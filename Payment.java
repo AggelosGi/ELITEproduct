956341f09579224c56c8a3e0c66783e83695d217
AggelosGi <83583424+AggelosGi@users.noreply.github.com>
2021-05-28 17:43:19 +0300

    Add files via upload

--- Payment.java
+++ Payment.java
@@ -1,4 +1,74 @@
 
 public class Payment {
 
+	private string card_name;
+	private int card_id;
+	private int cvv;
+	private DateTime date;
+	private string paypal_mail;
+	private string pass;
+	private double delivery_charges;
+	
+	
+	public retCreditCardFields () {
+		
+	}
+	
+	public retPaypalFields () {
+		
+	}
+	
+	public retPayonDeliveryCosts () {
+		
+	}
+	
+	private activateCheckoutMessage() {
+		
+	}
+	
+	public retCheckoutMessage() {
+		
+	}
+	
+	private selectPaymentoptions() {
+		
+	}
+	
+	public selectCreditCard(){
+		
+	}
+	
+	public select Paypal() {
+		
+	}
+	
+	public selectPayonDelivery() {
+		
+	}
+	
+	private acceptTermsofUse() {
+		
+	}
+	
+	private rejectTermsofUse() {
+		
+	}
+	
+	public getCreditCardFields() {
+		
+	}
+	
+	public getPaypalFields() {
+		
+	}
+	
+	public getPayonDeliveryCosts() {
+		
+	}
+	
+	public getCheckoutMessage() {
+		
+	}
+	
+	
 }
