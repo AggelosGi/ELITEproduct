a258fe3cd027055bd89152fd0439d79d3dc8e879
PanagiotaKapniari <83371953+PanagiotaKapniari@users.noreply.github.com>
2021-05-30 17:56:03 +0300

    Add files via upload

--- Producer.java
+++ Producer.java
@@ -1,5 +1,41 @@
+import java.util.*;
 
-public class Producer {
+public class Producer extends User {
+	
+	public String placed_products[];
+	
+	private void selectRegistration() {
+		
+	}
+	
+	private void addInfo() {
+		
+	}
+	
+	private void addPassword() {
+		
+	}
+	
+	private void addProducts() {
+		
+	}
+	
+	private void addProductSummary() {
+		
+	}
+	
+	private void addPhoto() {
+		
+	}
+	
+	private void addProducersSummary() {
+		
+	}
+	
+	private void addCertificates() {
+		
+	}
+
 
 
 }
--- ProducersAccount.java
+++ ProducersAccount.java
@@ -1,5 +1,49 @@
-
 public class ProducersAccount {
+	
+	public String name[];
+	private String address[];
+	private int phone[];
+	private String town[];
+	private int zip[];
+	public String personal_info[];
+	public String product_info[];
+	private String WarningMessage[];
+	
+	private void updatedInfoFields() {
+		
+	}
+	
+	private void updatePasswordField() {
+		
+	}
+	
+	private void updateProducersProducts() {
+		
+	}
+	
+	private void updateProductSummary() {
+		
+	}
+	
+	private void updateProducersPhoto() {
+		
+	}
+	
+	private void updateProducersSummary() {
+		
+	}
+	
+	private void updateCertificates() {
+		
+	}
+	
+	public void retProducersSummary() {
+		
+	}
+	
+	public void getProducersSummary() {
+		
+	}
 
 	
 }
