f5e398e4162fe4c6aa7fdd911d33ec17e4b670d7
stathis789 <34920611+stathis789@users.noreply.github.com>
2021-06-03 13:56:10 +0300

    Add files via upload

--- Evaluation.java
+++ Evaluation.java
@@ -3,4 +3,36 @@ import java.util.*;
 
 public class Evaluation {
 
+	private String purchased_products[]; //products that the buyer has purchased
+	public int grade_of_products[]; //
+	public String comment_of_products[];
+	
+	
+	public void retListofProducersEvalNot() {
+		
+	}
+	
+	private void updatedListofProducersEvalNot() {
+		
+	}
+	
+	public void typeComment() {
+		
+	}
+	
+	public void selectStar() {
+		
+	}
+	
+	public void selectProductRating() {
+		
+	}
+	
+	public void getListofProducersEvalNot() {
+		
+	}
+	
+	public void getListofProducersDepoNot() {
+		
+	}
 }
