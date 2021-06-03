dc47ed3547c522a7e5e8ac1c3d21706ef1b8c941
stathis789 <34920611+stathis789@users.noreply.github.com>
2021-06-03 14:00:12 +0300

    Add files via upload

--- Administrator.java
+++ Administrator.java
@@ -4,4 +4,50 @@ public class Administrator {
 
 	private String email;
 	private String pass;
+	
+	public void selectUpdateAssortment() {
+		
+	}
+	
+	public void selectDiscAssortment() {
+		
+	}
+	
+	public void selectUpdate() {
+		
+	}
+	
+	public void selectBacktoUpdatedCatalog() {
+		
+	}
+	
+	public void selectFindBestSeller() {
+		
+	}
+	
+	public void selectRegisterBestSeller() {
+		
+	}
+	
+	public void CancelBestSeller() {
+		
+	}
+	
+	public void selectEvaluations() {
+		
+	}
+	
+	public void selectNotificatedProducers_Ev() {
+		
+	}
+	
+	public void selectUnotificatedProducers_Eval() {
+		
+	}
+	
+	public void selectNotification() {
+		
+	}
+	
+	
 }
